5f2b74feb057a31f47081085a4e0277faad5af74
Michele <146134678+mxcvele@users.noreply.github.com>
2025-01-06 15:56:57 -0800

    updates to 2d game portfolio

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I program mostly in C#, JavaScript/TypeScript these days.
+      I'm working on a couple niche projects which I'm excited to share in the near future! I also like C++ and Python. Anywho regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/mxcvele" target="_blank">GitHub</a>!`,
+    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    "sofa-table": `That's my cozy sofa. I like to chillax here and watch shows/movies. 
+    I also love playing video games :D Follow <a href="https://www.twitch.tv/mxcvele" target="_blank">my twitch</a>! (If you like gaming)`,
+    tv: `That's my TV. Lately I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@mkbhd" target="_blank">mkbhd</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://ee46c99b-7cda-4a49-9bc3-083e4f5bdecd.filesusr.com/ugd/0f4d80_8e203653b70e41eca8d1acc2e4b2cbd1.pdf" target="_blank">Check it out?</a>
+    Connect with me at michvnguyen@gmail.com, if you have any interesting job opportunities!`,
+    projects: `About this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. To learn more about me, please check out <a href="https://michvnguyen.wixsite.com/portfolio" target="_blank">here</a>.`,
+    library: `There are a lot of comic books, graphic novels, and programming books on my shelves. My all time favorite comic book is Calvin and Hobbes as that is all that I read during my childhood growing up!`,
+    exit: `If you want to exit mxcvele's portfolio, just close the tab. Thank you for your time!`,
+  };
